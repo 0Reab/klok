a712760b0ca1ad1cbcfa7fdf7f79f95ea3db8129
0Reab <mihajlofrancic@gmail.com>
2023-10-10 20:18:54 +0200

    xd

--- add_time.py
+++ add_time.py
@@ -1,7 +1,23 @@
 def add_time(x, y, **kwargs):
     x = x.split()
-    print(x,y)
     day = kwargs
+    time_1 = x[0]
+    (time_1_hour,
+     time_2_hour,
+     time_1_minute,
+     time_2_minute) = time_1[:1:], y[:1:], time_1[2::], y[2::]
+
+    add_hour = int(time_1_hour) + int(time_2_hour)
+    add_minute = int(time_1_minute) + int(time_2_minute)
+    if add_hour > 12:
+        add_hour = 12 - add_hour
+    if add_minute == 60:
+        add_hour + 1
+        add_minute = 0
+    elif add_minute > 60:
+        add_minute = 60 - add_minute
+
+    print(f'{add_hour}:{add_minute} {x[1]}')
 
 
 
