df105c9858798597c05573667d19279f7cc9243f
0Reab <mihajlofrancic@gmail.com>
2023-10-13 23:58:04 +0200

    xd

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -5,12 +5,13 @@ def arithmetic_arranger(x,bool):
     print(lst)
 
     for i in lst:
-        for x in i:
-            if x[1] == "+":
-                op = {'+': lambda x, y: x + y,
-                      '-': lambda x, y: x - y}
+        for z in i:
+            result = list()
+            op = {'+': lambda x, y: x + y,
+                 '-': lambda x, y: x - y}
+            res = op['+'](z[0], z[2])
 
-                print(op['+'](1, 2))
+            print(result)
 
 
 print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
