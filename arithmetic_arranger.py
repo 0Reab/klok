59de820449d33c9592273041a5e78f68beb69d56
0Reab <mihajlofrancic@gmail.com>
2023-10-18 23:10:31 +0200

    xd

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -13,45 +13,35 @@ def arithmetic_arranger(x, bool):
 
         if len(x) > 5:
             print("Error: Too many problems.")
-        ic(lst)
         #for loop x2, i pretvaram z na indeksu 0 i 2 u ints ako to ne mogu return error
+
         for i in lst:
-            for z in i:
-                ic(z[0])
 
-            ic(type(i[0]))
-            # if not all(isinstance(z, int) for z in [int(i[0]), int(i[2])]):
-            #     print("Error: Numbers must only contain digits.")
-            #     break
-
-            val1 = int(i[0])
-            val2 = int(i[2])
-            oper = i[1]
-
-
-            if oper == ("+", "-"):
-                print("Error: Operator must be '+' or '-'.")
+            try:
+                val1 = int(i[0])
+            except:
+                print("Error: Numbers must only contain digits.")
                 break
 
-            # ic(type(val1))
-            # ic(type(val2))
-            #
-            # ic(isinstance([val1, val2], int))
-            # ic(isinstance(val1, int))
-            #ic(not all(isinstance(i, int) for i in [val1, val2]))
+            try:
+                val2 = int(i[2])
+            except:
+                print("Error: Numbers must only contain digits.")
+                break
 
+            oper = i[1]
 
-            # if isinstance((val1, val2), int) != True:
-            #     print("Error: Numbers must only contain digits.")
-            #     break
-
-            else:
+            try:
                 operat = {'+': lambda x, y: x + y,
                           '-': lambda x, y: x - y}
                 result.append(operat[i[1]](int(i[0]), int(i[2])))
 
+            except:
+                print("Error: Operator must be '+' or '-'.")
+                break
+
+
         if bool == True:
             print(f'')
 
-
-print(arithmetic_arranger(["a + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True))
+print(arithmetic_arranger(["1 + 8", "1 + 3801", "9999 + 9999", "523 - 49"], True))
